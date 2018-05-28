a6c5f4f3c851df9fc52689f294d4edb6b3388602
庞壮壮 <1197823941@qq.com>
2018-05-28 12:12:06 +0800

    no message

---
+++ src/main/java/ch/rasc/extclassgenerator/test/Person.java
@@ -0,0 +1,15 @@
+package ch.rasc.extclassgenerator.test;
+
+import ch.rasc.extclassgenerator.Model;
+import ch.rasc.extclassgenerator.ModelAssociation;
+import ch.rasc.extclassgenerator.ModelAssociationType;
+
+@Model(value = "MyApp.Person")
+public class Person {
+	private String name1222;
+	private String age;
+	private String sex;
+	private String phoneNum;
+	@ModelAssociation(value = ModelAssociationType.HAS_ONE)
+	private bb bb;
+}
